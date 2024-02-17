389973b31d3439edc4c8bcae91a09ae48260f1ac
Shotaro Ozawa <ozawa.shotaro0114@gmail.com>
2024-02-17 21:21:37 +0900

    init

---
+++ src/routes/__root.tsx
@@ -0,0 +1,39 @@
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+import { Suspense, lazy } from "react";
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null // Render nothing in production
+    : lazy(() =>
+        // Lazy load in development
+        import('@tanstack/router-devtools').then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        })),
+      )
+
+
+export const Route = createRootRoute({
+  component: Component
+})
+
+function Component() {
+  return (
+    <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{' '}
+        <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+      <Suspense>
+        <TanStackRouterDevtools />
+      </Suspense>
+    </>
+    )
+}
